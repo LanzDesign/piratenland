efe3d0c829b7db83ba8fd50e5358e40e1da460c7
LanzDesign <128187584+LanzDesign@users.noreply.github.com>
2023-08-23 16:43:03 +0200

    setup gh-pages

--- src/components/javascript/Home.jsx
+++ src/components/javascript/Home.jsx
@@ -1,9 +1,6 @@
 import '../css/root.css'
-import Navbar from './Navbar'
 import Header from './Header.js'
 import React from 'react';
-import MainContent from './MainContent';
-import SideButtons from './sidebuttons';
 import Wartung from './wartungsseite';
 import Footer from './footer'
 
